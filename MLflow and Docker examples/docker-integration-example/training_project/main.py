84829187f506f60e5806b90ff7a3e0abddeb4630
Chauhan Chirag <chiragchauhan1030@gmail.com>
2022-07-04 18:10:50 +0530

    onnx update

--- MLflow and Docker examples/docker-integration-example/training_project/main.py
+++ MLflow and Docker examples/docker-integration-example/training_project/main.py
@@ -3,10 +3,6 @@ import click
 import os
 
 def _run(entrypoint, parameters={}, source_version=None, use_cache=True):
-    #existing_run = _already_ran(entrypoint, parameters, source_version)
-    #if use_cache and existing_run:
-    #    print("Found existing run for entrypoint=%s and parameters=%s" % (entrypoint, parameters))
-     #   return existing_run
     print("Launching new run for entrypoint=%s and parameters=%s" % (entrypoint, parameters))
     submitted_run = mlflow.run(".", entrypoint, parameters=parameters)
     return mlflow.tracking.MlflowClient().get_run(submitted_run.run_id)
